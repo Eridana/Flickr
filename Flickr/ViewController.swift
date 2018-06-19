623ed82712f1a5f98e4de054431977e1aa421d5b
Jenya <evgenia.mikhailova@cuciniale.eu>
2018-06-19 10:06:21 +0200

    Initial Commit

---
+++ Flickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flickr
+//
+//  Created by Evgeniia Mikhailova on 19.06.18.
+//  Copyright © 2018 evgeniya.mikhailova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
